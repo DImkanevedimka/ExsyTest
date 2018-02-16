870a19294b63adbafb5b7bb2f1cbe738189e4f6a
DImkanevedimka <Dimkanevedimka33@gmail.com>
2018-02-16 12:11:24 +0200

    for check

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,59 +1,58 @@
 $(document).ready(function(){
-  $('.slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows:true,
-    draggable: false,
-    responsive: [
-      {
-        breakpoint: 720,
-        settings: {
-          arrows: false,
-          swipe: false
-        }
-      }
-    ]
-  });
-  
-  
-  $(".header-menu").on("click", "a", function (event) {
-    event.preventDefault();
-    var id  = $(this).attr('href');
-    var top = $(id).offset().top;
-    $('body, html').animate({scrollTop: top}, 1000);
-  });
-  
-  
-  
-  var popup = $('.popup');
-  var form = $('form');
-  var old = form.html();
-  
-  
-  $('.phone-button').on('click', function(){
-    form.html(old);
-    popup.removeClass('passive');
-    popup.addClass('active');
-  });
-  
-  popup.on('click', function(event){
-    if(event.target === popup[0] || event.target === $('.buttonClose')[0]){
-      popup.addClass('passive');
-      popup.removeClass('active');
-    };
-  });
-  
-  form.on('submit', function(event){
-    console.log(this[0].value)
-    form.html('<div class="message"><p>Благодарим ' + this[0].value + '.</p>'+
-    '<p> Мы перезвоним вам в кратчайшие сроки</p></div><div class="buttonClose">&#10006;</div>');
-  });
-  
-  
-  $('#toggle').click(function() {
-    $( this ).toggleClass( "on" );
-    $('.togglemenu').toggleClass('menu-on');
-  });
+	$('.slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows:true,
+		draggable: false,
+		responsive: [
+			{
+				breakpoint: 720,
+				settings: {
+					arrows: false,
+					swipe: false
+				}
+			}
+		]
+	});
+	
+	$(".header-menu").on("click", "a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href');
+		var top = $(id).offset().top;
+		$('body, html').animate({scrollTop: top}, 1000);
+	});
+	
+	var popup = $('.popup');
+	var form = $('.popup-form');
+	var popupMessage = $('.popup-message');
+	
+	$('.header-menu__phone-button').on('click', function(){
+		popup.removeClass('passive');
+		popup.addClass('active');
+	});
+	
+	function closePopup(){
+		popup.addClass('passive');
+		popup.removeClass('active');
+		popupMessage.removeClass('active');
+		popupMessage.addClass('popup-message_passive');
+		$('.popup-form__input').val('');
+	};
+
+	$('.popup__button-close').on('click',closePopup);
+	$('.popup-overlay').on('click', closePopup);
+	
+	form.on('submit', function(event){
+		event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+		popupMessage.removeClass('popup-message_passive');
+		popupMessage.addClass('active');
+		$('.popup-message__username').text(this[0].value);
+	});
+	
+	$('#toggle').click(function() {
+		$( this ).toggleClass( "on" );
+		$('.header-menu').toggleClass('header-menu-on');
+	});
 });
 
--- dist/js/map.js
+++ dist/js/map.js
@@ -1,85 +1,25 @@
-$(document).ready(function(){
-    google.maps.event.addDomListener(window, 'load', initMap);
-    function initMap() {
-        var mapOptions = {
-            
-            zoom: 13.1,
-            
-            center: new google.maps.LatLng(54.210764, 36.615503),
-            mapTypeControl: false,
-            zoomControl: false,
-            streetViewControl: false,
-            fullscreenControl: false,
-            styles: [
-                {
-                    "featureType": "water",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "hue": "#aacbd9"
-                        },
-                        {
-                            "visibility": "on"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "water",
-                    "elementType": "labels.text",
-                    "stylers": [
-                        {
-                            "hue": "#6b8ea9",
-                        }
-
-                    ]
-                },
-                {
-                    "featureType": "road",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "hue": "#1CB2BD"
-                        },
-                        {
-                            "saturation": 40
-                        }
-                    ]
-                },
-                {
-                    "featureType": "landscape",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "hue": "#f4f0d3"
-                        },
-                        {
-                            "visibility": "on"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.highway",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "visibility": "on"
-                        }
-                    ]
-                }
-                
-
-            ]
-        };
-        var mapElement = document.getElementById('map');
-        
-        
-        var map = new google.maps.Map(mapElement, mapOptions);
-        
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(54.210764, 36.615503),
-            map: map,
-            title: 'Snazzy!',
-            icon: '/img/icon.png'
-        });
-    };
+$(document).ready(function(){ 
+	google.maps.event.addDomListener(window, 'load', init);
+	
+	function init() {
+		var mapOptions = {
+			zoom: 13.1,
+			mapTypeControl: false,
+			zoomControl: false,
+			streetViewControl: false,
+			fullscreenControl: false,
+			center: new google.maps.LatLng(54.210764, 36.615503), 
+		};
+		
+		var mapElement = document.getElementById('map');
+		
+		var map = new google.maps.Map(mapElement, mapOptions);
+		
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(54.210764, 36.615503),
+			map: map,
+			title: 'Хатино',
+			icon: '/img/icon.png'
+		});
+	};
 });
